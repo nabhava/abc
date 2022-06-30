c1d8ff7cecb318b0e81bef13a3cc78b144806538
nabhava <105795595+nabhava@users.noreply.github.com>
2022-06-30 18:14:59 +0530

    Update RoundRobin.java

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,9 +1,3 @@
-/*DESCRIPTION
-Round Robin CPU Algorithm generally focuses on Time Sharing technique. 
-The period of time for which a process or job is allowed to run in a pre-emptive method is called time quantum. 
-Each process or job present in the ready queue is assigned the CPU for that time quantum, if the execution of the process is completed during that time then the process will end else the process will go back to the waiting table and wait for the its next turn to complete the execution.
-*/
-
 import java.util.Scanner;
 public class RoundRobin {
     public static void main(String args[]) {
