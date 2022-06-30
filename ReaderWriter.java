6414c232419f3880204b659d249e29039ef9cf27
nabhava <105795595+nabhava@users.noreply.github.com>
2022-06-30 18:14:34 +0530

    Update ReaderWriter.java

--- ReaderWriter.java
+++ ReaderWriter.java
@@ -1,11 +1,3 @@
-/*DESCRIPTION
-The readers-writers problem relates to an object such as a file that is shared between multiple processes. Some of these processes are readers i.e. they only want to read the data from the object and some of the processes are writers i.e. they want to write into the object.
-
-The readers-writers problem is used to manage synchronization so that there are no problems with the object data. For example - If two readers access the object at the same time there is no problem. However if two writers or a reader and writer access the object at the same time, there may be problems.
-
-To solve this situation, a writer should get exclusive access to an object i.e. when a writer is accessing the object, no reader or writer may access it. However, multiple readers can access the object at the same time.
-*/
-
 import java.util.concurrent.Semaphore;
 public class ReaderWriter {
     public static final int NUM_OF_READERS = 3;
