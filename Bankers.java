cc5c6dd83890f044b251943b94588d63aae92194
nabhava <105795595+nabhava@users.noreply.github.com>
2022-06-30 18:13:15 +0530

    Update Bankers.java

--- Bankers.java
+++ Bankers.java
@@ -1,7 +1,3 @@
-/*DESCRIPTION
-The banker’s algorithm is a resource allocation and deadlock avoidance algorithm that tests for safety by simulating the allocation for predetermined maximum possible amounts of all resources, then makes an “s-state” check to test for possible activities, before deciding whether allocation should be allowed to continue.
-*/
-
 import java.util.*;
 class Bankers {
     private int need[][], allocate[][], max[][], avail[][], total[][], np, nr;
